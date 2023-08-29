c7861ed6fafd6dbfb66e34ca733f2ff3ce005f18
은지 <dmswl6842@naver.com>
2023-08-29 18:44:18 +0900

    db주석

--- DontStopWeb/DontStop.py
+++ DontStopWeb/DontStop.py
@@ -4,7 +4,7 @@ import pymysql
 
 
 app = Flask(__name__)
-db = pymysql.connect(host='localhost', port=11245, user='RentalStart', passwd='vip0818!', db='RentalStart', charset='utf8')
+# db = pymysql.connect(host='localhost', port=11245, user='RentalStart', passwd='vip0818!', db='RentalStart', charset='utf8')
 
 
 @app.route('/', methods= ['POST','GET'])
